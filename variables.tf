9f4e620ae1948d260108367e2531277bf953b349
Kaivalya Kumar <81738764+KaivalyaKumar@users.noreply.github.com>
2025-12-08 18:23:02 +0530

    Add new complex variable 'keyvar' to variables.tf

--- variables.tf
+++ variables.tf
@@ -25,3 +25,27 @@ validation {
 error_message = "Must be between 10 and 1000"
 }
 }
+
+variable "keyvar" {
+    type = object({
+        name = string
+        size = optional(list(string),["xyz","23"])
+        dimensions = object({
+            len = optional(number,65/7)
+            bred = string
+            dims = map(object({
+                leng = optional(string,"247")
+                breadth = optional(list(string),["24*7"])
+            }))
+        })
+        tetrax = list(object({
+            len = string
+            bred = string
+            dims = object({
+                leng = string
+                breadth = optional(list(number),[007,1077.66,60-7])
+            })
+        }))
+    })
+    nullable = false
+}
